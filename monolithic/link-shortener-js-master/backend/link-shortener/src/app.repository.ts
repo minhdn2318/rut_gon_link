f9108aa07650dd025dfae95f78843c2a3020ed94
LeViet241 <leviet241@gmail.com>
2025-05-02 22:01:49 +0700

    remove old folder

--- monolithic/link-shortener-js-master/backend/link-shortener/src/app.repository.ts
+++
@@ -1,8 +0,0 @@
-import { Observable } from 'rxjs';
-
-export interface AppRepository {
-  put(hash: string, url: string): Observable<string>;
-  get(hash: string): Observable<string>;
-}
-
-export const AppRepositoryTag = 'AppRepository';
--- tuananh_layerstructure/src/config/index.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config(); // Đảm bảo dotenv được gọi ở đây nữa
-
-module.exports = {
-  port: process.env.PORT,
-  database: {
-    storage: process.env.DB_STORAGE,
-    dialect: 'sqlite' // Hoặc có thể lấy từ .env nếu muốn linh hoạt hơn
-  }
-};

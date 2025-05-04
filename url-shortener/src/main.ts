aeb9e10e8dccaa7f7048dc7b93f288d25702792e
LeViet241 <leviet241@gmail.com>
2025-05-04 17:59:22 +0700

    merge test-performance result

--- url-shortener/src/main.ts
+++ url-shortener/src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   app.enableCors();
-  await app.listen(configService.get<number>('PORT', 3000));
+  await app.listen(configService.get<number>('PORT', 5000));
 }
 bootstrap();

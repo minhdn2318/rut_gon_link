52de80383e1e5f97d779e50a7999e0e2cf333a7e
wliontb <wliontb@gmail.com>
2025-05-04 18:39:23 +0700

    add UI

--- client/src/app.service.ts
+++ client/src/app.service.ts
@@ -7,70 +7,85 @@ import { getConfig } from './common/config/configuration';
 
 @Injectable()
 export class AppService {
-
-  constructor(private readonly httpService: HttpService,
+  constructor(
+    private readonly httpService: HttpService,
     private readonly circuitBreakerService: CircuitBreakerService,
-    private readonly configService: ConfigService
+    private readonly configService: ConfigService,
   ) {}
 
-  async createShortUrl(originUrl : string) : Promise<string> {
+  async createShortUrl(originUrl: string): Promise<string> {
     const config = getConfig(this.configService);
     const url = `${config.server.doMain}/create`;
-    try 
-    {
-      if(config.circuitBreaker.circuitBreaker == true){
-          const response = await this.circuitBreakerService.execute(() =>
-          firstValueFrom(        
-            this.httpService.post(url, {
-            originalUrl: originUrl,
-          }, {
-            headers: { 'Content-Type': 'application/json' },
-          })
-        ));
+    try {
+      if (config.circuitBreaker.circuitBreaker == true) {
+        const response = await this.circuitBreakerService.execute(() =>
+          firstValueFrom(
+            this.httpService.post(
+              url,
+              {
+                originalUrl: originUrl,
+              },
+              {
+                headers: { 'Content-Type': 'application/json' },
+              },
+            ),
+          ),
+        );
         return response.data;
-      }
-      else{
-        const response = await firstValueFrom(        
-            this.httpService.post(url, {
-            originalUrl: originUrl,
-          }, {
-            headers: { 'Content-Type': 'application/json' },
-          })
+      } else {
+        const response = await firstValueFrom(
+          this.httpService.post(
+            url,
+            {
+              originalUrl: originUrl,
+            },
+            {
+              headers: { 'Content-Type': 'application/json' },
+            },
+          ),
         );
         return response.data;
       }
-
     } catch (error) {
-      console.error('Error:', error.response?.data || error.message);
+      // console.error('Error:', error.response?.data || error.message);
       throw error;
     }
   }
 
-  async getOriginUrl(shortUrl : string) : Promise<string> {
+  async getOriginUrl(shortUrl: string): Promise<string> {
     const config = getConfig(this.configService);
     const url = `${config.server.doMain}/short/${shortUrl}`;
-    try 
-    {
-      if(config.circuitBreaker.circuitBreaker == true){
-      const response = await this.circuitBreakerService.execute(() =>
-        firstValueFrom(this.httpService.get(url))
-      );
-      return response.data;
-      }
-      else {
+    try {
+      let responseResult = '';
+      if (config.circuitBreaker.circuitBreaker == true) {
+        const response = await this.circuitBreakerService.execute(() =>
+          firstValueFrom(this.httpService.get(url)),
+        );
+        responseResult = response.data;
+      } else {
         const response = await firstValueFrom(this.httpService.get(url));
-        return response.data;
+        responseResult = response.data;
       }
+
+      console.log('Response:', responseResult);
+      // Chỉ lấy phần sau short/ nếu response.data là một URL
+      if (typeof responseResult === 'string') {
+        const match = responseResult.match(/short\/(.+)$/);
+        return match ? match[1] : responseResult;
+      }
+      return responseResult;
     } catch (error) {
       console.error('Error:', error.response?.data || error.message);
       throw error;
     }
   }
 
-  async testCircuitBreaker() : Promise<string> {
+  async testCircuitBreaker(): Promise<string> {
     return this.circuitBreakerService.execute(() => {
       return new Promise((_, reject) => {
-        reject({ response: { status: 500, message: 'Simulated Server Error' } });
+        reject({
+          response: { status: 500, message: 'Simulated Server Error' },
+        });
       });
     });
   }
--- client/src/main.ts
+++ client/src/main.ts
@@ -1,8 +1,16 @@
+import { Module } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Cấu hình Handlebars làm template engine
+  app.setBaseViewsDir(join(__dirname, '..', 'src', 'views'));
+  app.setViewEngine('hbs');
+
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
